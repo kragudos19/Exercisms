40a344d381709b96c9295b7aade736ed5b13bd91
Kathryn Ragudos <kragudos19@gmail.com>
2016-10-29 08:40:27 -0700

    fixed dynamic constant error

--- hamming.rb
+++ hamming.rb
@@ -1,24 +1,29 @@
 # class Hamming iterates through a DNA strand to look at the levels of mutation
 
 class Hamming
-  LETTER_DIFFERENCE = 0
   def self.compute(str1, str2)
-    raise raise_exception if str_1.length != str_2.length
+    raise raise_exception if str1.length != str2.length
+    letter_difference = 0
     i = 0
-    while str_1.length > i 
-      distance(str1[i], str2[i])
+    while str1.length > i 
+      letter_difference += distance(str1[i], str2[i])
       i += 1
     end
+    p letter_difference
   end
 
   def self.distance(letter1, letter2)
-   LETTER_DIFFERENCE += 1 if letter1 != letter2
+    return 1 if letter1 != letter2
+    return 0
   end
 
   def self.raise_exception
-    ArgumentError.new('Wrong number of Arguments')
+     ArgumentError.new('Wrong number of Arguments')
   end
 end
+
 module BookKeeping
-  VERSION = 3
+    VERSION = 3
 end
+
+# Hamming.compute("GAGCCTACTAACGGGAT", "CATCGTAATGACGGCCT")
