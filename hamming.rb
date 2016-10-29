537e395c5a04fb7d591c36867881c384ee1c6456
Kathryn Ragudos <kragudos19@gmail.com>
2016-10-29 09:58:29 -0700

    reduced the number of lines and cleaned up code

--- hamming.rb
+++ hamming.rb
@@ -1,29 +1,21 @@
 # class Hamming iterates through a DNA strand to look at the levels of mutation
 
+module BookKeeping
+  VERSION = 3
+end
+
 class Hamming
+  VERSION = 3
   def self.compute(str1, str2)
     raise raise_exception if str1.length != str2.length
-    letter_difference = 0
-    i = 0
-    while str1.length > i 
-      letter_difference += distance(str1[i], str2[i])
-      i += 1
-    end
-    p letter_difference
-  end
-
-  def self.distance(letter1, letter2)
-    return 1 if letter1 != letter2
-    return 0
+    hamming_difference = 0
+    str1.length.times { |x| hamming_difference += 1 if str1[x] != str2[x] }
+    hamming_difference
   end
 
   def self.raise_exception
-     ArgumentError.new('Wrong number of Arguments')
+    ArgumentError.new('Wrong number of Arguments')
   end
 end
 
-module BookKeeping
-    VERSION = 3
-end
-
-# Hamming.compute("GAGCCTACTAACGGGAT", "CATCGTAATGACGGCCT")
+Hamming.compute('GAGCCTACTAACGGGAT', 'CATCGTAATGACGGCCT')
