1b0f6b84e8b566b063dd1e506fee2ede1fb1a184
Kathryn Ragudos <kragudos19@gmail.com>
2016-11-04 13:14:35 -0700

    fixed variables and deleted a squared value

--- difference-of-squares/difference_of_squares.rb
+++ difference-of-squares/difference_of_squares.rb
@@ -3,29 +3,36 @@ module BookKeeping
 end
 
 class Squares 
+  attr_reader :number
   def initialize(number)
-    self.number = number
+    @number = number
   end
 
   def difference
-    return 0 if self.number == 0
-    square_of_sums - sum_of_squares
+    return 0 if number == 0
+    number_conversion(square_of_sum - sum_of_squares)
   end
 
   def sum_of_squares
-    return 0 if self.number == 0
-    (1..self.number).reduce(1) { |sum, n| sum**2 + n**2 }
+    return 0 if number == 0
+    (1..@number).reduce(1) { |sum, n| sum + n**2 }
   end
 
-  def square_of_sums
-    return 0 if self.number == 0
-    ((1..self.number).reduce(:+))**2
+  def square_of_sum
+    return 0 if number == 0
+    ((1..@number).reduce(:+))**2
   end
 
   private
   def number_conversion(number_to_convert)
-    number_to_convert.each_with_index do |value, idx|
-      # if idx 
+    converted_number = ""
+    number_to_convert.to_s.split.each_with_index do |value, idx|
+      if (idx + 1) % 3 == 0
+        converted_number << "_"
+      else
+        converted_number << value
+      end
     end
+    return converted_number
   end
 end
